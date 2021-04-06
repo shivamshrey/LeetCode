484ad51a80010857255c95cd24a396e71d93517a
Shivam Shrey <31056227+shivamshrey@users.noreply.github.com>
2021-04-06 22:23:37 +0530

    Nineteenth Commit

---
+++ LC_240.py
@@ -0,0 +1,18 @@
+# 240. Search a 2D Matrix II
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row = len(matrix)
+        col = len(matrix[0])
+        i = 0
+        j = col - 1
+        
+        while i < row and j >= 0:
+            if target == matrix[i][j]:
+                return True
+            elif target > matrix[i][j]:
+                i += 1
+            else:
+                j -= 1
+        
+        return False
