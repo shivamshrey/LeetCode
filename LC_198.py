0083e27ee66cc3a2bbc1b323d0b2b8a4f5cee8d3
Shivam Shrey <31056227+shivamshrey@users.noreply.github.com>
2021-05-28 23:33:26 +0530

    Add LC 198

---
+++ LC_198.py
@@ -0,0 +1,18 @@
+# 198. House Robber
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        
+        dp = [0] * (n + 1)
+        dp[1] = nums[0]
+        dp[2] = max(nums[0], nums[1])
+        
+        for i in range(3, n + 1):
+            dp[i] = max(dp[i - 1],
+                        dp[i - 2] + nums[i - 1])
+        
+        return dp[n]
+    
