ac1480867b5708cbf5b351b4b21bdb43cc88e16c
Shivam Shrey <31056227+shivamshrey@users.noreply.github.com>
2021-07-31 00:43:08 +0530

    Add LC 49

---
+++ LC_49.py
@@ -0,0 +1,9 @@
+# 49. Group Anagrams
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+        for word in strs:
+            anagrams[''.join(sorted(word))].append(word)
+        return list(anagrams.values())
+    
