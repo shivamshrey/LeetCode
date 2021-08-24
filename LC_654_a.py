13ec4de97be068c7b1ce546e5a78a2518c214e5b
Shivam Shrey <31056227+shivamshrey@users.noreply.github.com>
2021-08-24 23:19:12 +0530

    Add LC 654, 701

---
+++ LC_654_a.py
@@ -0,0 +1,23 @@
+# 654. Maximum Binary Tree
+# TC: O(n^2)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
+        if not nums:
+            return None
+    
+        new_val = max(nums)
+        split_index = nums.index(new_val)
+        node = TreeNode(new_val)
+        if len(nums[:split_index]):
+            node.left = self.constructMaximumBinaryTree(nums[:split_index])
+        if len(nums[split_index + 1:]):
+            node.right = self.constructMaximumBinaryTree(nums[split_index + 1:])
+
+        return node
