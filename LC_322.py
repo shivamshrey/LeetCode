69cbad527559b6050189cebf7216d161cbca16df
Shivam Shrey <31056227+shivamshrey@users.noreply.github.com>
2021-11-24 00:38:35 +0530

    Add LC 322

---
+++ LC_322.py
@@ -0,0 +1,14 @@
+# 322. Coin Change
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+        
+        for i in range(1, amount + 1):
+            for j in range(len(coins)):
+                if coins[j] <= i:
+                    dp[i] = min(dp[i], dp[i - coins[j]] + 1)
+        
+        return dp[amount] if dp[amount] <= amount else -1
+    
