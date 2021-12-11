059e783a0f5393f0a3f57e606163e4dfeec68120
Shivam Shrey <31056227+shivamshrey@users.noreply.github.com>
2021-12-11 11:44:43 +0530

    Update LC 221

--- LC_221.java
+++ LC_221.java
@@ -2,7 +2,7 @@
 
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        // dp[i][j] = side length of max square that can be formed whose bottom right corner is (i, j) in the original matrix
+        // dp[i][j] = side length of max square that can be formed whose bottom right corner is (i - 1, j - 1) in the original matrix
         int[][] dp = new int[matrix.length + 1][matrix[0].length + 1];
         int maxLen = 0;
         for (int i = 1; i <= matrix.length; i++) {
