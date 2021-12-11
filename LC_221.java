27f117fe736c99d9ebf1d9b5181362b0239653ab
Shivam Shrey <31056227+shivamshrey@users.noreply.github.com>
2021-12-11 11:40:47 +0530

    Add LC 221, 64

---
+++ LC_221.java
@@ -0,0 +1,19 @@
+// 221. Maximal Square
+
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int[][] dp = new int[matrix.length + 1][matrix[0].length + 1];
+        int maxLen = 0;
+        for (int i = 1; i <= matrix.length; i++) {
+            for (int j = 1; j <= matrix[0].length; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], 
+                                        Math.min(dp[i - 1][j], dp[i][j - 1]))
+                                            + 1;
+                    maxLen = Math.max(maxLen, dp[i][j]);
+                }
+            }
+        }
+        return maxLen * maxLen;
+    }
+}
