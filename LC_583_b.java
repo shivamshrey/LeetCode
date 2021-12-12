1affa920dd7dade2cd0552ee325b369a78abe889
Shivam Shrey <31056227+shivamshrey@users.noreply.github.com>
2021-12-12 13:32:22 +0530

    Update LC 583

--- LC_583_b.java
+++ LC_583_b.java
@@ -5,25 +5,26 @@ class Solution {
         int m = word1.length();
         int n = word2.length();
         
-        int[][] lcs = new int[m + 1][n + 1];
+        int[][] dp = new int[m + 1][n + 1];
         
         for (int i = 0; i <= m; i++)
-            lcs[i][0] = 0;
+            dp[i][0] = 0;
         
         for (int j = 0; j <= n; j++)
-            lcs[0][j] = 0;
+            dp[0][j] = 0;
         
         for (int i = 0; i <= m; i++) {
             for (int j = 0; j <= n; j++) {
                 if (i == 0 || j == 0) {
-                    lcs[i][j] = i + j;
-                } else if (word1.charAt(i - 1) == word2.charAt(j - 1))
-                    lcs[i][j] = lcs[i - 1][j - 1];
-                else
-                    lcs[i][j] = 1 + Math.min(lcs[i - 1][j], lcs[i][j - 1]);
+                    dp[i][j] = i + j;
+                } else if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1]);
+                }
             }
         }
         
-        return lcs[m][n];
+        return dp[m][n];
     }
 }
