241e34b473658688f4a38f1e4aa8d94ab05e3d15
Shivam Shrey <31056227+shivamshrey@users.noreply.github.com>
2021-12-27 11:38:15 +0530

    Add LC 476

---
+++ LC_476.py
@@ -0,0 +1,11 @@
+# 476. Number Complement
+
+class Solution:
+    def findComplement(self, num: int) -> int:
+        i = 1
+        
+        while i <= num:
+            i = i << 1
+        
+        return (i - 1) ^ num
+    
