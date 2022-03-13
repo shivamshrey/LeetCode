f8bfe8ee2266a2623ab07109691d066726663b21
Shivam Shrey <31056227+shivamshrey@users.noreply.github.com>
2022-03-13 13:13:29 +0530

    Update LC 20

--- LC_20.py
+++ LC_20.py
@@ -2,20 +2,14 @@ class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
         for char in s:
-            if char in ['(', '[', '{']:
+            if char in "{[(":
                 stack.append(char)
-            else:
-                # stack can't be empty when a closing bracket is encountered
-                if not stack:
+            elif not stack or not self.__isMatch(stack.pop(), char):
                     return False
-                elif not self.is_matching_pair(stack[-1], char):
-                    return False
-                else:
-                    stack.pop()
-        # if stack is not empty, then parantheses are not balanced
-        return False if stack else True
         
-    def is_matching_pair(self, a, b):
+        return not stack
+        
+    def __isMatch(self, a, b):
         return a == '(' and b == ')' \
             or a == '[' and b == ']' \
             or a == '{' and b == '}'
