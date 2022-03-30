7bd090ed648220c6ad8b30daa4030099fe855362
Shivam Shrey <31056227+shivamshrey@users.noreply.github.com>
2022-03-30 23:53:36 +0530

    Add LC 32

---
+++ LC_32.py
@@ -0,0 +1,19 @@
+# 32. Longest Valid Parentheses
+# https://leetcode.com/problems/longest-valid-parentheses/
+
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        stack = [-1]
+        result = 0
+        for i, char in enumerate(s):
+            if char == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    result = max(result, i - stack[-1])
+        
+        return result                   
+                
