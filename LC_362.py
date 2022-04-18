9a4e3420101e53754e73322f5a8266378a33295f
Shivam Shrey <31056227+shivamshrey@users.noreply.github.com>
2022-04-18 14:06:24 +0530

    Add LC 362

---
+++ LC_362.py
@@ -0,0 +1,20 @@
+# 362. Design Hit Counter
+# https://leetcode.com/problems/design-hit-counter/
+
+class HitCounter:
+
+    def __init__(self):
+        self.hits = collections.deque()
+
+    def hit(self, timestamp: int) -> None:
+        self.hits.append(timestamp)
+
+    def getHits(self, timestamp: int) -> int:
+        while self.hits and self.hits[0] <= timestamp - 300:
+            self.hits.popleft()
+        return len(self.hits)
+
+# Your HitCounter object will be instantiated and called as such:
+# obj = HitCounter()
+# obj.hit(timestamp)
+# param_2 = obj.getHits(timestamp)
