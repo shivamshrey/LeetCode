5f013e1dc8f1f714e47d380dd40b56079e0dafed
Shivam Shrey <31056227+shivamshrey@users.noreply.github.com>
2022-05-01 11:08:05 +0530

    Update LC 2262

--- LC_2262.py
+++ LC_2262.py
@@ -3,14 +3,12 @@
 
 class Solution:
     def appealSum(self, s: str) -> int:
-        n = len(s)
         last_seen_index = dict()
-        ans = 0
-        for i, c in enumerate(s):
-            if c not in last_seen_index:
-                ans += (i + 1) * (n - i)
-            else:
-                ans += (i - last_seen_index[c]) * (n - i)
-            last_seen_index[c] = i
+        result, count = 0, 0
         
-        return ans
+        for i, c in enumerate(s):
+            count += i - last_seen_index.get(c, -1)
+            last_seen_index[c] = i
+            result += count
+        
+        return result
